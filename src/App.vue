9a1402abe82633fdf6539d61dd4f06699971944e
Latisha Washington <lwashing@stu.southtexascollege.edu>
2026-09-13 15:31:08 -0500

    Added Sections

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import Header from './components/Header.vue'
-import Card from './components/Card.vue'
+  import Header from './components/Header.vue'
+  import Card from './components/Card.vue'
+  import Section from './components/Section.vue'
 </script>
 
 <template>
@@ -16,28 +17,25 @@ import Card from './components/Card.vue'
       <select id="paidBySelect"></select>
       <button>Add Expense</button>
     </form>
-    <section>
-      <h3>People</h3>
+
+    <Section title="People">
       <ul id="peopleList" class="list"></ul>
-    </section>
-    
-    <section>
-      <h3>Expenses</h3>
-      <ul id="expenseList" class="list"></ul>
-    </section>
-    
-    <section>
-      <h3>Total</h3>
-      <p>
-        Total Spent: <strong id="totalSpent">$0.00</strong><br/>
-        Split Per Person: <strong id="splitAmount">$0.00</strong>
-      </p>
-    </section>
-    
-    <section class="summary">
-      <h3>Summary</h3>
-      <ul id="summaryList" class="list"></ul>
-    </section>
+    </Section>
+
+    <Section title="Expenses">
+        <ul id="expenseList" class="list"></ul>
+    </Section>
+
+    <Section title="Total">
+        <p>
+            Total Spent: <strong id="totalSpent">$0.00</strong>
+            Split Per Person: <strong id="splitAmount">$0.00</strong>
+        </p>
+    </Section>
+
+    <Section title="Summary">
+        <ul id="summaryList" class="list"></ul>
+    </Section>
   </Card>
 </template>
 
